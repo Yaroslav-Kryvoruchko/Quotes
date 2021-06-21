ffcb95e7a7ca9783c78244daaa30b3bcc2fc067e
Yaroslav <yaroslavkrivoruchko01@gmail.com>
2021-06-20 17:44:05 -0700

    Quotes

--- app.js
+++ app.js
@@ -1,49 +1,83 @@
+const times = document.getElementById("times");
+const quoteItemTop = document.getElementById("quote_item_top");
+const quoteItemBottom = document.getElementById("quote_item_bottom");
+
 let numberQuotes;
-let times = document.getElementById("times");
-let timerTime = 0.1 * 60; /* 0.1 = 6 seconds, 1 = minute, 1440 = 24 hour */
+let userInputTime = 0.1; /* parseInt(prompt("What you want set time on your timer?", "1440")) */ /* 0.1 = 6 seconds, 1 = minute, 1440 = 24 hour */
+let timerTime = userInputTime * 60;
+let numberOfQuotes = 21;
+let checkNumber = [];
+
+// if (isNaN(userInputTime) || userInputTime <= 0) {
+//   timerTime = 1440 * 60;
+// }
+
+function addAndCheckNumbers() {
+  for (let i = 0; i < numberOfQuotes; i++) {
+    checkNumber.push(Math.trunc(Math.random() * numberOfQuotes));
+  }
+
+  checkNumber = checkNumber.filter(
+    (item, index) => checkNumber.indexOf(item) == index
+  );
+
+  console.log(checkNumber);
+
+  checkArrayLength();
+}
+
+addAndCheckNumbers();
+
+function checkArrayLength() {
+  if (checkNumber.length < numberOfQuotes) {
+    addAndCheckNumbers();
+  }
+}
+
+let i = 0;
+
+function randomNumbers() {
+  numberQuotes = checkNumber[i];
+  i++;
+
+  if (i === numberOfQuotes) {
+    checkNumber = [];
+    addAndCheckNumbers();
+    i = 0;
+  }
+}
 
 function timerMain() {
   timer = setInterval(() => {
-    seconds = timerTime % 60;
-    minutes = (timerTime / 60) % 60;
-    hour = (timerTime / 60 / 60) % 60;
+    seconds = Math.trunc(timerTime % 60);
+    minutes = Math.trunc((timerTime / 60) % 60);
+    hour = Math.trunc((timerTime / 60 / 60) % 60);
 
-    if (timerTime <= 0) {
-      timerTime = timerTime + 0.1 * 60;
-      numberQuotes = Math.trunc(Math.random() * 3);/* number 3 answers for quantity randoms numbers */
-      console.log(numberQuotes);
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+
+    if (timerTime == 0) {
+      randomNumbers();
+      timerTime = timerTime + userInputTime * 60;
       changeQuotes(numberQuotes);
     } else {
-      let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${Math.trunc(seconds)}`;
-      times.innerHTML = strTimer;
+      let strTimer = `${hour}:${minutes}:${seconds}`;
+      times.textContent = strTimer;
     }
     --timerTime;
   }, 1000);
 }
 
-const quoteItemTop = document.getElementById("quote_item_top");
-const quoteItemBottom = document.getElementById("quote_item_bottom");
-
-const arroyQuotes = [
-  {
-    mainText:
-      "While we are postponing, life speeds",
-    secondaryText: "by. Seneca",
-  },
-  {
-    mainText:
-      "The first half of life consists of the capacity to enjoy without the chance, the last half consists of the chance without the capacity.",
-    secondaryText: "by. Mark Twain",
-  },
-  {
-    mainText: "If you do not think about the future, you cannot have one.",
-    secondaryText: "by. Galsworthy",
-  },
-];
+timerMain();
 
 function changeQuotes(numberQuote) {
-  quoteItemTop.textContent = arroyQuotes[numberQuote].mainText;
-  quoteItemBottom.textContent = arroyQuotes[numberQuote].secondaryText;
+  quoteItemTop.innerHTML = arroyQuotes[numberQuote].mainText;
+  quoteItemBottom.innerHTML = arroyQuotes[numberQuote].secondaryText;
 }
-
-timerMain();
---
+++ arrayQuotes.js
@@ -0,0 +1,101 @@
+const arroyQuotes = [
+  {
+    mainText: "While we are postponing, life speeds",
+    secondaryText: "by. Seneca",
+  },
+  {
+    mainText:
+      "The first half of life consists of the capacity to enjoy without the chance, the last half consists of the chance without the capacity.",
+    secondaryText: "by. Mark Twain",
+  },
+  {
+    mainText: "If you do not think about the future, you cannot have one.",
+    secondaryText: "by. Galsworthy",
+  },
+  {
+    mainText: "The roots of education are bitter, but the fruit is sweet.",
+    secondaryText: "by. Aristotle",
+  },
+  {
+    mainText: "The great aim of education is not knowledge but action.",
+    secondaryText: "by. Herbert Spencer",
+  },
+  {
+    mainText: "Education is a progressive discovery of our own ignorance.",
+    secondaryText: 'by. William James "Will" Durant',
+  },
+  {
+    mainText: "Always forgive your enemies; nothing annoys them so much.",
+    secondaryText: "by. Oscar Wilde",
+  },
+  {
+    mainText:
+      "A banker is a fellow who lends you his umbrella when the sun is shining, but wants it back the minute it begins to rain.",
+    secondaryText: "by. Mark Twain",
+  },
+  {
+    mainText: "I am not young enough to know everything.",
+    secondaryText: "by. Oscar Wilde",
+  },
+  {
+    mainText:
+      "It is with our passions, as it is with fire and water, they are good servants but bad masters.",
+    secondaryText: "by. Aesop",
+  },
+
+  {
+    mainText: "An investment in knowledge always pays the best interest.",
+    secondaryText: "by. Benjamin Franklin",
+  },
+  {
+    mainText:
+      "I like pigs. Dogs look up to us. Cats look down on us. Pigs treat us as equals.",
+    secondaryText: "by. Sir Winston Churchill",
+  },
+  {
+    mainText:
+      "Abortion is advocated only by persons who have themselves been born.",
+    secondaryText: "by. Ronald Reagan",
+  },
+  {
+    mainText:
+      "Own only what you can carry with you; know language, know countries, know people. Let your memory be your travel bag.",
+    secondaryText: "by. Alexander Solzhenitsyn",
+  },
+  {
+    mainText:
+      "The very first law in advertising is to avoid the concrete promise and cultivate the delightfully vague.",
+    secondaryText: "by. Bill Cosby",
+  },
+  {
+    mainText:
+      "Logic will get you from A to B. Imagination will take you anywhere.",
+    secondaryText: "by. Albert Einstein",
+  },
+  {
+    mainText:
+      "Our greatest glory is not in never falling, but in getting up every time we do.",
+    secondaryText: "by. Confucius",
+  },
+  {
+    mainText:
+      "Political language is designed to make lies sound truthful and murder respectable and to give an appearance of solidity to pure wind.",
+    secondaryText: "by. George Orwell",
+  },
+  {
+    mainText:
+      "Translation is like a woman. If it is beautiful, it is not faithful. If it is faithful, it is most certainly not beautiful.",
+    secondaryText: "by. Yevgeny Yevtushenko",
+  },
+  {
+    mainText:
+      "England and America are two countries separated by the same language.",
+    secondaryText: "by. George Bernard Shaw",
+  },
+  {
+    mainText: "Adults are just outdated children.",
+    secondaryText: "by. Dr. Seuss",
+  },
+];
+
+const arrayStartQuotes = [];
